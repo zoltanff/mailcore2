7c5281912b1a081c4da818028712284fceb187bb
Zoltan Foley-Fisher <zoltanff@gmail.com>
2020-10-14 19:25:16 -0700

    Exclude older iOS versions, 32-bit simulator and armv7/armv7s

--- scripts/include.sh/build-dep.sh
+++ scripts/include.sh/build-dep.sh
@@ -6,10 +6,10 @@ build_git_ios()
     return
   fi
 
-  simarchs="i386 x86_64"
-  sdkminversion="7.0"
+  simarchs="x86_64"
+  sdkminversion="12.4"
   sdkversion="`xcodebuild -showsdks 2>/dev/null | grep iphoneos | sed 's/.*iphoneos\(.*\)/\1/'`"
-  devicearchs="armv7 armv7s arm64"
+  devicearchs="arm64"
 
   versions_path="$scriptpath/deps-versions.plist"
   version="`defaults read "$versions_path" "$name" 2>/dev/null`"
